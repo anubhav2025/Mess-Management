77c5a5b7bdc3ac36877d74a5c40f2ea2de9acf11
Anubhav Pandey <akp99376@gmail.com>
2023-11-14 22:28:23 +0530

    testing

---
+++ backend/data/roleIndexData.js
@@ -0,0 +1,31 @@
+import bcrypt from "bcryptjs";
+
+const roleindexes = [
+  {
+    email: "accountant@gmail.com",
+    messId: "655274b2951333d3004ce568",
+    role: "accountant",
+  },
+  {
+    email: "admin@mnnit.com",
+    messId: "655274b2951333d3004ce568",
+    role: "storeKeeper",
+  },
+  {
+    email: "student@gmail.com",
+    messId: "655274b2951333d3004ce568",
+    role: "student",
+  },
+  {
+    email: "studentmanager@gmail.com",
+    messId: "655274b2951333d3004ce568",
+    role: "studentMessManager",
+  },
+  {
+    email: "warden@gmail.com",
+    messId: "655274b2951333d3004ce568",
+    role: "wardern",
+  },
+];
+
+export default roleindexes;
--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,161 +1,179 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 // import { useDispatch, useSelector } from 'react-redux';
-import { useState, useEffect } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import Loader from '../components/Loader';
-import { toast } from 'react-toastify';
-import { useLoginMutation } from '../slices/userApiSlice';
-import { setCredentials } from '../slices/authSlice';
-import { useDispatch, useSelector } from 'react-redux';
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import Loader from "../components/Loader";
+import { toast } from "react-toastify";
+import { useLoginMutation } from "../slices/userApiSlice";
+import { setCredentials } from "../slices/authSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 function Copyright(props) {
-   return (
-      <Typography variant="body2" color="text.secondary" align="center" {...props}>
-         {'Copyright © '}
-         <Link to="/" style={{ color: '#1976d2', textDecoration: 'none' }}>
-            Mess Manager
-         </Link>{' '}
-         {new Date().getFullYear()}
-         {'.'}
-      </Typography>
-   );
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link to="/" style={{ color: "#1976d2", textDecoration: "none" }}>
+        Mess Manager
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
 
 const defaultTheme = createTheme();
 
 export default function LoginScreen() {
-   const [email, setEmail] = useState('');
-   const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-   const dispatch = useDispatch();
-   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-   const [login, { isLoading }] = useLoginMutation();
+  const [login, { isLoading }] = useLoginMutation();
 
-   const { userInfo } = useSelector((state) => state.auth);
+  const { userInfo } = useSelector((state) => state.auth);
 
-   const { search } = useLocation();
-   const sp = new URLSearchParams(search);
-   const redirect = sp.get('redirect') || '/';
+  const { search } = useLocation();
+  const sp = new URLSearchParams(search);
+  const redirect = sp.get("redirect") || "/";
 
-   useEffect(() => {
-      if (userInfo) {
-         navigate(redirect);
-      }
-   }, [navigate, redirect, userInfo]);
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
 
-   const submitHandler = async (e) => {
-      e.preventDefault();
-      console.log(email);
-      console.log(password);
-      try {
-         const res = await login({ email, password }).unwrap();
-         dispatch(setCredentials({ ...res }));
-         navigate(redirect);
-      }
-      catch (err) {
-         toast.error(err?.data?.message || err.error);
-      }
-   };
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+    try {
+      const res = await login({ email, password }).unwrap();
+      dispatch(setCredentials({ ...res }));
+      navigate(redirect);
+    } catch (err) {
+      toast.error(err?.data?.message || err.error);
+    }
+  };
 
-   return (
-      <ThemeProvider theme={defaultTheme}>
-         <Grid container component="main" sx={{ height: '100vh' }}>
-            <CssBaseline />
-            <Grid
-               item
-               xs={false}
-               sm={4}
-               md={7}
-               sx={{
-                  backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
-                  backgroundRepeat: 'no-repeat',
-                  backgroundColor: (t) =>
-                     t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-                  backgroundSize: 'cover',
-                  backgroundPosition: 'center',
-               }}
-            />
-            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-               <Box
-                  sx={{
-                     my: 8,
-                     mx: 4,
-                     display: 'flex',
-                     flexDirection: 'column',
-                     alignItems: 'center',
-                  }}
-               >
-                  <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                     <LockOutlinedIcon />
-                  </Avatar>
-                  <Typography component="h1" variant="h5">
-                     Sign in
-                  </Typography>
-                  <Box component="form" noValidate onSubmit={submitHandler} sx={{ mt: 1 }}>
-                     <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                     />
-                     <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                     />
-                     <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
-                     >
-                        Sign In
-                     </Button>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage:
+              "url(https://source.unsplash.com/random?wallpapers)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={submitHandler}
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
 
-                     {isLoading && <Loader />}
+              {isLoading && <Loader />}
 
-                     <Grid container>
-                        <Grid item xs>
-                           <Link to={"/login"} style={{ color: '#1976d2', textDecoration: 'none' }}>
-                              Forgot password?
-                           </Link>
-                        </Grid>
-                        <Grid item>
-                           <Link to={"/register"} style={{ color: '#1976d2', textDecoration: 'none' }}>
-                              Don't have an account? Sign Up
-                           </Link>
-                        </Grid>
-                     </Grid>
-                     <Copyright sx={{ mt: 5 }} />
-                  </Box>
-               </Box>
-            </Grid>
-         </Grid>
-      </ThemeProvider>
-   );
+              <Grid container>
+                <Grid item xs>
+                  <Link
+                    to={"/login"}
+                    style={{ color: "#1976d2", textDecoration: "none" }}
+                  >
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link
+                    to={"/register"}
+                    style={{ color: "#1976d2", textDecoration: "none" }}
+                  >
+                    Don't have an account? Sign Up
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
 }
